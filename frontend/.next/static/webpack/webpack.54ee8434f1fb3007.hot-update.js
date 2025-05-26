bb2ae92745f2690bc9a10d99beb41e678aca9083
Ayush Kumar Suryan <100873752+ayushsuryan@users.noreply.github.com>
2025-05-26 20:16:44 +0530

    FIxes for them to work simultaneously

--- frontend/.next/static/webpack/webpack.54ee8434f1fb3007.hot-update.js
+++ frontend/.next/static/webpack/webpack.54ee8434f1fb3007.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9931fb88efd39c24"; }
+/******/ 	__webpack_require__.h = function() { return "78658438d55ba7d5"; }
 /******/ }();
 /******/ 
 /******/ }
